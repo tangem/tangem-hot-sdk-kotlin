61e51c7db4884da2a28909d904c7d228c3a65f60
Tangem <git@tangem.com>
2025-05-23 11:45:04 +0300

    Updated on 2025-11-24

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("configuration")
 }
 
 android {
@@ -31,7 +32,6 @@ android {
         jvmTarget = "11"
     }
 }
-
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
