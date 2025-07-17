d413fe3f58385c20ccc130782465dc07f7d6d53f
Tangem <git@tangem.com>
2025-07-17 16:15:43 +0300

    Updated on 2026-07-29

--- android/src/main/java/com/tangem/hot/sdk/android/exception/AllowedExceptions.kt
+++ android/src/main/java/com/tangem/hot/sdk/android/exception/AllowedExceptions.kt
@@ -1,7 +1,15 @@
 package com.tangem.hot.sdk.android.exception
 
+import com.tangem.common.core.TangemSdkError
 import com.tangem.hot.sdk.exception.WrongPasswordException
 
 internal fun isAllowedException(exception: Throwable): Boolean {
-    return exception is WrongPasswordException
+    return exception is WrongPasswordException ||
+            exception is TangemSdkError.AuthenticationCanceled ||
+            exception is TangemSdkError.AuthenticationFailed ||
+            exception is TangemSdkError.AuthenticationLockout ||
+            exception is TangemSdkError.AuthenticationUnavailable ||
+            exception is TangemSdkError.AuthenticationAlreadyInProgress ||
+            exception is TangemSdkError.AuthenticationNotInitialized ||
+            exception is TangemSdkError.AuthenticationPermanentLockout
 }
